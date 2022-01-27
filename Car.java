7930618df5125a52718dac88c0f02fb953ada199
rajihariinfotech <98318860+rajihariinfotech@users.noreply.github.com>
2022-01-27 06:49:00 -0800

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class Car {
+
+	int regnNumber=7989;
+	long insuranceNo=678935467899L;
+	float chasisNumber=3457897799F;
+	short fuelCapacity=25;
+	double engineOilCapacity=5698.35485969;
+	boolean isTirePuncture=true;
+	char varient1='p';
+	char varient2='d';
+	char varient3='e';
+
+			}
